f1be1209c5ca870621cd3772e34f9d0e40550d59
gugunguntoro <ibuisme@gmail.com>
2025-05-30 00:35:47 +0700

    Add files via upload

---
+++ hai.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Aplikasi Gugun Men")
+
+nama = st.text_input("Masukkan nama kamu :)")
+
+if st.button("Cek"):
+    if not nama:
+        st.warning("Masukkan nama kamu dulu dong")
+    else:
+        st.success(f"Hai {nama}, aku Gugun Men, aku siap membantu kamu untuk membasmi penjahat dimuka bumi ini. Tapi aku makan dulu, ya... Kamu juga makan, karena melawan penjahat butuh energi.")
+    
+
